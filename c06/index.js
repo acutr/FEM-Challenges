8488847063e7d6ea9ad87d97baffa408d7e37f3e
acutr <118777107+acutr@users.noreply.github.com>
2023-01-03 11:58:13 +0800

    WIP

--- c06/index.js
+++ c06/index.js
@@ -13,38 +13,3 @@ navToggle.addEventListener("click", () => {
     }
 })
 
-
-// var romanToInt = function (s) {
-//     let numArray = [];
-//     let sum = 0;
-//     romArray = s.split('');
-//     console.log(romArray.length)
-//     for (i = 0; i < romArray.length; i++) {
-//         if (romArray[i] == "I") {
-//             numArray[i] = 1;
-//         } else if (romArray[i] == "V") {
-//             numArray[i] = 5;
-//         } else if (romArray[i] == "X") {
-//             numArray[i] = 10;
-//         } else if (romArray[i] == "L") {
-//             numArray[i] = 50;
-//         } else if (romArray[i] == "C") {
-//             numArray[i] = 100;
-//         } else if (romArray[i] == "D") {
-//             numArray[i] = 500;
-//         } else if (romArray[i] == "M") {
-//             numArray[i] = 1000;
-//         }
-//         console.log(numArray)
-//         if (i > 0 && numArray[i] > numArray[i - 1]) {
-//             numArray[i] -= numArray[i - 1];
-//             sum -= numArray[i - 1];
-//         }
-//         sum += numArray[i]
-//         console.log("part sum = " + numArray[i]);
-//     }
-//     console.log("hi")
-//     return sum
-// };
-// 
-// console.log(romanToInt("MCMXCIV"))
